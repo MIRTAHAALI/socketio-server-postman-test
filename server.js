2028f53b70e59adf4f49008262921e0174e031f9
MIR TAHA ALI <mirtahaali23@yahoo.com>
2022-11-01 17:29:35 +0500

    First Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+	console.log('Connected');
+
+	socket.on('msg', function(from,msg){
+		console.log('Message is '+from, msg);
+	})
+	socket.on('disconnect', function(msg){
+		console.log('Disconnected');
+	})
+});
+
+http.listen(3000, function(){
+	console.log('Listening to port 3000');
+})
